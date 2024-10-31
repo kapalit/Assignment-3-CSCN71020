9153298ad106103829301baa2a9c3f7517197541
Mohamed Al-Husainawi <Malhusainawi6279@conestogac.on.ca>
2024-10-30 22:26:26 -0400

    added a test to check that the match ends with a draw

--- UnitTest_RPS/UnitTest_RPS.cpp
+++ UnitTest_RPS/UnitTest_RPS.cpp
@@ -13,8 +13,21 @@ namespace UnitTest
 		TEST_METHOD(TestPlayer1Wins)
 		{
 			char* result = rockPaperScissors("Rock", "Scissors");
-			Assert::AreEqual("Player1", result); // test case of Player1 winning 
+			Assert::AreEqual("Player1", result); // Player1 should win this case
 		}
+		
+		TEST_METHOD(TestPlayer2Wins)
+		{
+			char* result = rockPaperScissors("Rock", "Paper");
+			Assert::AreEqual("Player2", result); // Player2 should win this case
+		}
+		
+		TEST_METHOD(TestDraw)
+		{
+			char* result = rockPaperScissors("Scissors", "Scissors");
+			Assert::AreEqual("Draw", result); // This should end in a draw
+		}
+
 
 	};
 }
