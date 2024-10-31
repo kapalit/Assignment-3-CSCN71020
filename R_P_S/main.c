fdc21c286f6f381867bc939c9931d39ce26c2dc0
Mohamed Al-Husainawi <Malhusainawi6279@conestogac.on.ca>
2024-10-30 23:09:50 -0400

    Added game logic to determine rock-paper-scissors outcome

--- R_P_S/main.c
+++ R_P_S/main.c
@@ -2,14 +2,29 @@
 
 char* rockPaperScissors(const char* player1, const char* player2) {
 
-	int valid_player1 = strcmp(player1, "Rock") == 0 || strcmp(player1, "Paper") == 0 || strcmp(player1, "Scissors") == 0;
+	int valid_player1 = strcmp(player1, "Rock") == 0 ||
+		strcmp(player1, "Paper") == 0 ||
+		strcmp(player1, "Scissors") == 0;
 
-	int valid_player2 = strcmp(player2, "Rock") == 0 || strcmp(player2, "Paper") == 0 || strcmp(player2, "Scissors") == 0;
+	int valid_player2 = strcmp(player2, "Rock") == 0 ||
+		strcmp(player2, "Paper") == 0 ||
+		strcmp(player2, "Scissors") == 0;
 
 	if (!valid_player1 || !valid_player2) {
 		return "invalid";
 	}
 
-	
+	if (strcmp(player1, player2) == 0) {
+		return "Draw";
+	}
+	else if ((strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0) ||
+		(strcmp(player1, "Scissors") == 0 && strcmp(player2, "Paper") == 0) ||
+		(strcmp(player1, "Paper") == 0 && strcmp(player2, "Rock") == 0)) {
+		return "player1";
+	}
+	else {
+		return "player2";
+	}
+
 
 }
